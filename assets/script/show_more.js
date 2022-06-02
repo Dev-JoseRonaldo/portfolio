d60ab377aff768ff5d5d8a1515469343d4261d38
José Ronaldo <dev.joseronaldo@gmail.com>
2022-06-01 21:47:28 -0300

    :sparkles: feat: Estrutura, estilização e javascript do botão 'mostrar mais projetos' e 'mostrar menos projetos'

---
+++ assets/script/show_more.js
@@ -0,0 +1,28 @@
+let loadMoreBtn = document.querySelector('#load-more')
+let hiddenMoreBtn = document.querySelector('#hidden-more')
+let currentItem = 6
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.items__box')]
+  for (var i = currentItem; i < currentItem + 6 && i < boxes.length; i++) {
+    boxes[i].style.display = 'inline-block'
+  }
+  currentItem += 6
+
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = 'none'
+    hiddenMoreBtn.style.display = 'inline-block'
+  }
+}
+
+hiddenMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.items__box')]
+  for (let i = boxes.length - 1; i >= 6; i--) {
+    console.log(boxes[i])
+    boxes[i].style.display = 'none'
+  }
+  currentItem = 6
+
+  loadMoreBtn.style.display = 'inline-block'
+  hiddenMoreBtn.style.display = 'none'
+}
