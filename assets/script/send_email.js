0c9164f35f9944b36396a35006d9cc7d3411c3e9
José Ronaldo <dev.joseronaldo@gmail.com>
2022-06-03 13:45:42 -0300

    :recycle: refactor: Arrumando o email de recebimento das mensagens e o corpo do email

--- assets/script/send_email.js
+++ assets/script/send_email.js
@@ -6,15 +6,6 @@ btn.addEventListener('click', function (e) {
   var email = document.getElementById('email').value
   var subject = document.getElementById('subject').value
   var message = document.getElementById('message').value
-  var body =
-    'name:' +
-    name +
-    '</br> email:' +
-    email +
-    '</br> subject:' +
-    subject +
-    '</br> message:' +
-    message
 
   console.log('h1')
   Email.send({
@@ -22,8 +13,11 @@ btn.addEventListener('click', function (e) {
     Username: 'dev.joseronaldo@gmail.com',
     Password: '90C0578FDC10A0D7EE275F99480FB80B8261',
     To: 'dev.joseronaldo@gmail.com',
-    From: email,
+    From: 'dev.joseronaldo@gmail.com',
     Subject: subject,
-    Body: body
-  }).then(message => alert(message))
+    Body: `Name: ${name} <br/> 
+    Email: ${email} <br/> <br/> 
+    Subject: ${subject} <br/> 
+    Message: ${message}`
+  }).then(message => alert('Email enviado com sucesso'))
 })
