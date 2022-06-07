13bf4e0bf21960b223ca122c456802e97afd9577
José Ronaldo <dev.joseronaldo@gmail.com>
2022-06-07 00:11:39 -0300

    :bug: fix: Resolvendo bug de scroll da página desabilitado em momentos errados

--- assets/script/toggle.js
+++ assets/script/toggle.js
@@ -1,4 +1,4 @@
-let show = true
+let show = false
 
 const menuSection = document.querySelector('.header__menu-section')
 const menuToggle = document.querySelector('.header__menu-toggle')
@@ -7,6 +7,7 @@ const buttonsToggle = document.querySelectorAll('.header__item')
 
 /* Exibe o menu mobile (se tiver oculto) e desativa a rolagem da página (se tiver ativada),
  ou esconde o menu mobile (se tiver visível) e ativa a rolagem da página (se tiver desativada), ao clickar no ícone do menu mobile */
+
 menuToggle.addEventListener('click', () => {
   document.body.style.overflow = show ? 'hidden' : 'initial'
   menuSection.classList.toggle('on', show)
@@ -14,9 +15,10 @@ menuToggle.addEventListener('click', () => {
 })
 
 /* apenas Esconde o menu mobile (se tiver visível) e ativa a rolagem da página (se tiver desativada), ao clickar nos links de navegação  */
+
 for (let i = 0; i < buttonsToggle.length; i++) {
   buttonsToggle[i].addEventListener('click', () => {
-    document.body.style.overflow = show ? 'hidden' : 'initial'
+    document.body.style.overflow = 'initial'
     menuSection.classList.toggle('on', show)
     show = !show
   })
